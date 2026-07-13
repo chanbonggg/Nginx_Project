e2bd4bc21dd0d0efb2b7633e1dec8f3d471580bd
Chanbonggg <22011705@sju.ac.kr>
2026-07-13 20:13:11 +0900

    spring-backend를 dockercopose로 실행

--- spring-backend/src/main/java/com/project/spring_backend/HealthController.java
+++ spring-backend/src/main/java/com/project/spring_backend/HealthController.java
@@ -1,4 +1,4 @@
-package com.chan.springbackend;
+package com.project.spring_backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
